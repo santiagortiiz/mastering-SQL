8bed8de1fa590b0828a108f12c7b0c1ae51b0a6d
santiago.ortiz <santiago.ortiz@iotaimpact.com>
2022-05-20 16:30:48 -0500

    release guide for dashboards data

---
+++ sql/development/santiago/dashboards_release_guide.sql
@@ -0,0 +1,38 @@
+-- 1. Delete updated_by and created_by fields in the
+--    dashboards tables of the development environment.
+UPDATE dashboards
+SET created_by_id=NULL, updated_by_id=NULL
+
+UPDATE dashboards_cards
+SET created_by_id=NULL, updated_by_id=NULL
+
+UPDATE dashboards_tabs
+SET created_by_id=NULL, updated_by_id=NULL
+
+-- 3. Take individual backups of the following tables in the dev environment:
+--      * dashboards
+--      * dashboards_tabs
+--      * dashboards_cards
+--      * dashboards_datasets
+--      * datasets_catalog
+
+-- 4. Take a backup of the main database
+
+-- 5. Delete all dashboards data in the following order
+DELETE FROM dashboards_datasets
+DELETE FROM datasets_catalog
+DELETE FROM dashboards_cards
+DELETE FROM dashboards_tabs
+
+UPDATE products
+SET dashboard_id=NULL
+
+DELETE FROM dashboards
+SELECT * FROM products WHERE dashboard_id IS NOT NULL
+
+-- 6. Restore the development backups of each table in main in the following order:
+--      * dashboards
+--      * dashboards_tabs
+--      * dashboards_cards
+--      * datasets_catalog
+--      * dashboards_datasets
